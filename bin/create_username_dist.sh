97851e93c82a01ccbfc67a71823dd2a603eb5f1e
david <lucht013@morris.umn.edu>
2020-09-06 13:02:13 -0500

    Repair create_username_dist.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-
 #CONSTANTS
 directory_name=$1
 here=$(pwd)
-
 #MAIN
 cd "$directory_name" || exit
+cat ./*/failed_login_data.txt | awk '{print $4}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > username_dist_temp.txt
 
-cat ./failed_login_data.txt | sort | awk '{print $4}' | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | username_dist_temp.html
-
-cd "$here" || exit
-
-./bin/wrap_contents.sh 
+"$here"/bin/wrap_contents.sh username_dist_temp.txt "$here"/html_components/username_dist username_dist.html
