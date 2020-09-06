c398de04e04755408038c973ec545a721e06f307
david <lucht013@morris.umn.edu>
2020-09-06 11:16:53 -0500

    Fix process_client_logs.sh so last test doesn't fail

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,6 +1,6 @@
 TARGET=$1
-cd "$TARGET"/var/log
-cat * | awk '{FS = "[: ]"}; /Failed password for invalid user|Failed password for/{
+cd "$TARGET" || exit
+cat var/log/* | awk '{FS = "[: ]"}; /Failed password for invalid user|Failed password for/{
 	if($2=="")
 		 print $1,$3,$4,$(NF-5),$(NF-3);
 	 else
