c0a0f167bade35c43687550de2addaf249d1ce17
david <lucht013@morris.umn.edu>
2020-09-08 12:09:22 -0500

    Implement functional create_country_dist.sh

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -4,9 +4,11 @@ directory_name=$1
 here=$(pwd)
 #MAIN
 cd "$directory_name" || exit
-cat ./*/failed_login_data.txt | awk '{print $3}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > hours_dist_temp.txt
 
-"$here"/bin/wrap_contents.sh hours_dist_temp.txt "$here"/html_components/hours_dist hours_dist.html
+# next line looks at data, passes to awk to find ip's, sorts those ips, adds country code, counts unique entries, prints out formatted country code data
+cat ./*/failed_login_data.txt | awk '{print $5}'| sort | join - "$here"/etc/country_IP_map.txt | awk '{print $2};' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > country_dist_temp.txt
 
-#Cleaning Temp files
-rm hours_dist_temp.txt
+"$here"/bin/wrap_contents.sh country_dist_temp.txt "$here"/html_components/country_dist country_dist.html
+
+#Cleaning Temp File
+rm country_dist_temp.txt
