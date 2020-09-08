05d26572b7228a532c7905e9707d0a0f1beb2d50
david <lucht013@morris.umn.edu>
2020-09-08 10:03:11 -0500

    Duplicate create_hours_dist.sh and rename to create_country.sh

---
+++ bin/create_country_dist.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#CONSTANTS
+directory_name=$1
+here=$(pwd)
+#MAIN
+cd "$directory_name" || exit
+cat ./*/failed_login_data.txt | awk '{print $3}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > hours_dist_temp.txt
+
+"$here"/bin/wrap_contents.sh hours_dist_temp.txt "$here"/html_components/hours_dist hours_dist.html
+
+#Cleaning Temp files
+rm hours_dist_temp.txt
