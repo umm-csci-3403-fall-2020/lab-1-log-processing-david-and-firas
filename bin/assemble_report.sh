ae165435dd2c4e0a1490b68fa62abd408f6b9e90
david <lucht013@morris.umn.edu>
2020-09-09 18:40:30 -0500

    Implement assemble_report.sh

---
+++ bin/assemble_report.sh
@@ -0,0 +1,10 @@
+#CONSTANTS
+TARGET=$1
+HERE=$(pwd)
+
+cd "$TARGET"
+
+cat country_dist.html hours_dist.html username_dist.html > temp.html
+"$HERE"/bin/wrap_contents.sh temp.html "$HERE"/html_components/summary_plots failed_login_summary.html
+
+rm temp.html
