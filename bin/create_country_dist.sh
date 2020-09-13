b7acdbff8fb59c55566b922c277e0db1b4800fda
david <lucht013@morris.umn.edu>
2020-09-12 19:33:33 -0500

    Add functional process_logs.sh and fix bugs in create_x_dist.sh

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -2,13 +2,14 @@
 #CONSTANTS
 directory_name=$1
 here=$(pwd)
+BIN="$(dirname "$(readlink -f $0)")"
 #MAIN
 cd "$directory_name" || exit
 
 # next line looks at data, passes to awk to find ip's, sorts those ips, adds country code, counts unique entries, prints out formatted country code data
-cat ./*/failed_login_data.txt | awk '{print $5}'| sort | join - "$here"/etc/country_IP_map.txt | awk '{print $2};' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > country_dist_temp.txt
+cat ./*/failed_login_data.txt | awk '{print $5}'| sort | join - "$BIN"/../etc/country_IP_map.txt | awk '{print $2};' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > country_dist_temp.txt
 
-"$here"/bin/wrap_contents.sh country_dist_temp.txt "$here"/html_components/country_dist country_dist.html
+"$BIN"/wrap_contents.sh country_dist_temp.txt "$BIN"/../html_components/country_dist country_dist.html
 
 #Cleaning Temp File
 rm country_dist_temp.txt
--- bin/create_username_dist (copy).sh
+++ bin/create_username_dist (copy).sh
@@ -2,12 +2,12 @@
 #CONSTANTS
 directory_name=$1
 here=$(pwd)
-BIN="$(dirname "$(readlink -f $0)")"
 #MAIN
 cd "$directory_name" || exit
 # cat looks at data, passes to awk to find 
 cat ./*/failed_login_data.txt | awk '{print $4}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > username_dist_temp.txt
-"$BIN"/wrap_contents.sh username_dist_temp.txt "$BIN"/../html_components/username_dist username_dist.html
+
+"$here"/bin/wrap_contents.sh username_dist_temp.txt "$here"/html_components/username_dist username_dist.html
 
 #Cleaning Temp File
 rm username_dist_temp.txt
